2a273c73eeb505a8629d48c182e94943cb381da3
ekinnard23 <ekinnard@gmail.com>
2023-10-03 19:34:44 -0500

    Question 6

--- scripts/data_analyst_jobs_ekinnard23.sql
+++ scripts/data_analyst_jobs_ekinnard23.sql
@@ -20,17 +20,31 @@ LIMIT 10;
 
 SELECT COUNT (location)
 FROM data_analyst_jobs
-WHERE location = 'KY' OR 'TN'
+WHERE location = 'KY' OR location = 'TN'
 
+--27
 
 -- 4.	How many postings in Tennessee have a star rating above 4?
 SELECT COUNT (location)
 FROM data_analyst_jobs
 WHERE location = 'TN' and star_rating > 4
 
+--3
+
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
+SELECT COUNT (review_count)
+FROM data_analyst_jobs
+WHERE review_count Between 500 AND 1000
+
+--151
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
+
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+GROUP BY location
+
+
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
